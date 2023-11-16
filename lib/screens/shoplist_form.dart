f9e46a7dea0b07b9e4c0e718cf5198d4ca6d4f0e
gregoriussamuelh@gmail.com <gregoriussamuelh@gmail.com>
2023-11-16 11:36:45 +0700

    final commit for tutorial 8

--- lib/screens/shoplist_form.dart
+++ lib/screens/shoplist_form.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shopping_list/screens/menu.dart';
 import 'package:shopping_list/widgets/left_drawer.dart'; // TODO: Import the previously created drawer
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
 
 class ShopFormPage extends StatefulWidget {
     const ShopFormPage({super.key});
@@ -15,6 +20,7 @@ class _ShopFormPageState extends State<ShopFormPage> {
   String _description = "";
     @override
     Widget build(BuildContext context) {
+      final request = context.watch<CookieRequest>();
         return Scaffold(
   appBar: AppBar(
     title: const Center(
@@ -115,37 +121,36 @@ class _ShopFormPageState extends State<ShopFormPage> {
             backgroundColor:
               MaterialStateProperty.all(Colors.indigo),
             ),
-          onPressed: () {
-          if (_formKey.currentState!.validate()) {showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: const Text('Product successfully saved'),
-            content: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment:
-                    CrossAxisAlignment.start,
-                children: [
-                  Text('Name: $_name'),
-                  Text('Price: $_price'),
-                  Text('Description: $_description')
-                  // TODO: Display other values
-                ],
-              ),
-            ),
-            actions: [
-              TextButton(
-                child: const Text('OK'),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          );
-        },
-      );
-    _formKey.currentState!.reset();}
-          },
+          onPressed: () async {
+          if (_formKey.currentState!.validate()) {
+        // Send request to Django and wait for the response
+        // TODO: Change the URL to your Django app's URL. Don't forget to add the trailing slash (/) if needed.
+        final response = await request.postJson(
+        "http://127.0.0.1:8000/create-flutter/",
+        jsonEncode(<String, String>{
+            'name': _name,
+            'price': _price.toString(),
+            'description': _description,
+            // TODO: Adjust the fields with your Django model
+        }));
+        if (response['status'] == 'success') {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(const SnackBar(
+            content: Text("New product has saved successfully!"),
+            ));
+            Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => MyHomePage()),
+            );
+        } else {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(const SnackBar(
+                content:
+                    Text("Something went wrong, please try again."),
+            ));
+        }
+    }
+},
           child: const Text(
           "Save",
           style: TextStyle(color: Colors.white),
--- lib/widgets/shop_card.dart
+++ lib/widgets/shop_card.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/screens/login.dart';
 import 'package:shopping_list/screens/shoplist_form.dart';
+import 'package:shopping_list/screens/list_product.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
 
 class ShopItem {
   final String name;
@@ -14,11 +18,12 @@ class ShopCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
     return Material(
       color: Colors.indigo,
       child: InkWell(
         // Responsive touch area
-        onTap: () {
+        onTap: () async {
           // Show a SnackBar when clicked
           ScaffoldMessenger.of(context)
             ..hideCurrentSnackBar()
@@ -32,6 +37,30 @@ class ShopCard extends StatelessWidget {
               builder: (context) => ShopFormPage(),
         ));          // TODO: Use Navigator.push to navigate to a MaterialPageRoute that encompasses ShopFormPage.
             } 
+            else if (item.name == "View Products") {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const ProductPage()));
+      }
+      else if (item.name == "Logout") {
+        final response = await request.logout(
+            // TODO: Change the URL to your Django app's URL. Don't forget to add the trailing slash (/) if needed.
+            "http://127.0.0.1:8000/auth/logout/");
+        String message = response["message"];
+        if (response['status']) {
+          String uname = response["username"];
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text("$message Good bye, $uname."),
+          ));
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => const LoginPage()),
+          );
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text("$message"),
+          ));
+        }
+      }
   },
         
         
